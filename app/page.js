e7044f486e2eead56bec6985ef63f40f5cf646f4
Sarthak0203 <138149235+Sarthak0203@users.noreply.github.com>
2024-07-01 19:50:40 +0530

    underlinedText bugs fixed

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import TopBanner from "./components/TopBanner";
+import UnderlinedText from "@/utils/UnderlinedText";
 
 export default function Home() {
   return (
     <>
-    <UnderlinedText text={"hello"} active/>
     <TopBanner />
     <Navbar />
+    <UnderlinedText text={"hello"} />
     </>
     
   );
--- utils/UnderlinedText.js
+++ utils/UnderlinedText.js
@@ -4,8 +4,10 @@ export default function UnderlinedText({ text, active }) {
   const underlineClass = active ? 'bg-[length:100%_2px]' : 'group-hover:bg-[length:100%_2px]';
 
   return (
-    <span className={`bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat transition-all duration-500 ease-out ${underlineClass}`}>
-      {text}
-    </span>
+    <div className='group inline-block'>
+      <span className={`bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat transition-all duration-500 ease-out ${underlineClass}`}>
+        {text}
+      </span>
+    </div>
   );
 }
